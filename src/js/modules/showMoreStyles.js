a32cb258951e40bfb34df99a2818510fad42c3a7
SvonkLoki <bot_on_war3@mail.ru>
2021-04-01 21:45:41 +0300

    Some mod

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -4,44 +4,25 @@ const showMoreStyles = (trigger, wrapper) => {
     'use strict';
     const btn = document.querySelector(trigger);
 
-    // cards.forEach(card => {
-    //     card.classList.add('animated', 'fadeInUp');
-    // });
-
-    // btn.addEventListener('click', () => {
-    //     cards.forEach(card => {
-    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-    //     });
-    //     btn.remove();
-    // });
-
-    //     <div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
-    // <div class=styles-block>
-    //     <img src=assets/img/styles-5.jpg alt>
-    //     <h4>Пастелью</h4>
-    //     <a href="#">Подробнее</a>
-    // </div>
-    // </div>
     btn.addEventListener('click', function() {
-        getResource('http://localhost:3000/styles')
-            .then(res => createCards(res))
-            .then(error => console.log(error));
+        getResource('assets/db.json')
+            .then(res => createCards(res.styles))
+            .catch(error => console.log(error));
 
         this.remove();
     });
 
     function createCards(response) {
-        response.forEach(({ src, title, link }) => {
+        response.forEach((item) => {
             let card = document.createElement('div');
 
             card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
 
             card.innerHTML = `
             <div class="styles-block">
-                <img src="${src}" alt="style">
-                <h4>${title}</h4>
-                <a href="${link}">Подробнее</a>
+                <img src="${item.src}" alt="style">
+                <h4>${item.title}</h4>
+                <a href="${item.link}">Подробнее</a>
             </div>
             `;
 
